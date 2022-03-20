492034c07fb590232c9517710f429b3995ea41ae
20kzhan <49377371+20kzhan@users.noreply.github.com>
2022-03-20 12:37:25 -0700

    Delete Wordle directory

--- Wordle/wordle.py
+++
@@ -1,8 +0,0 @@
-with open("word_list.txt.txt", 'r') as word_list:
-    word_list = [s.replace('\n', '') for s in word_list.readlines()]
-with open("all_words.txt", 'r') as valid_words:
-    valid_words = [w.lower() for w in valid_words.readlines()[0].split(' ')]
-    valid_words[-1] = valid_words[-1]
-
-guess = input("Enter the first guess: ")
-correct_letters =
